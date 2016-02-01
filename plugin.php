8e2aa6ce532f946122d5cd8e8b12273d7153ed6e
Nick Breen <nick@foobar.net.nz>
2016-02-01 18:35:20 +1300

    added fields to admin edit order screen, fixed meta storage

--- plugin.php
+++ plugin.php
@@ -22,10 +22,14 @@ $fields = array(
   ),
 );
 
-// We'll put out new fields into the checkout form's billing section
+// We'll put out new fields into the checkout form's $form section
+$form = 'order';
 $form_fields = array(
-  'billing' => $fields,
+  $form => $fields,
 );
+
+// Label for the admin edit order section
+$order_details_label = 'Order Details';
 
 // Validation filters for our new fields.
 $field_filters = array(
@@ -34,7 +38,7 @@ $field_filters = array(
     'flags' => FILTER_REQUIRE_SCALAR,
     'options' => function ($value) {
       // if the string does not contain anything unusual
-      return $value === filter_var($value, FILTER_SANITIZE_STRING) ? $value : FALSE;
+      return filter_var($value, FILTER_SANITIZE_STRING) === $value ? $value : FALSE;
     },
     'notice' => sprintf('<strong>%s</strong> is invalid.', $fields['purchase_order_reference']['label']),
   ),
@@ -48,20 +52,33 @@ add_action('woocommerce_checkout_process', function () use ($field_filters) {
   $filtered_fields = filter_input_array(INPUT_POST, $field_filters, TRUE);
   foreach ($filtered_fields as $field => $result)
     // $result === NULL if unspecified, which is OK
-    if ($result === FALSE) // if filter failed, which is not OK
+    if (FALSE === $result) // if filter failed, which is not OK
       wc_add_notice($field_filters[$field]['notice'], 'error');
 });
 
 add_action( 'woocommerce_checkout_update_order_meta', function ($order_id) use ($fields, $field_filters) {
   $filtered_fields = filter_input_array(INPUT_POST, $field_filters, TRUE);
   foreach ($filtered_fields as $field => $result)
-    if ($result !== FALSE && $result !== NULL) {
-      update_post_meta($order_id, $field, serialize($result));
-      update_post_meta($order_id, $fields[$field]['label'], $result);
-    }
+    if (FALSE !== $result &&  NULL !== $result)
+      update_post_meta($order_id, $field, $result);
 });
 
-// TODO add to order received
-// TODO add to order confirmation email
-// TODO add to new order email
-// TODO add to order details box next to notes
+add_filter('woocommerce_email_order_meta_fields', function ($email_fields, $sent_to_admin, $order) use ($fields) {
+  foreach ($fields as $field => $def)
+    $email_fields[] = array(
+      'label' => $def['label'],
+      'value' => get_post_meta($order->id, $field, TRUE),
+    );
+  $email_fields[] = array('label'=>'Test Label', 'value' => 'Test Value');
+  return $email_fields;
+});
+
+add_action('woocommerce_admin_order_data_after_order_details', function ($order) use ($fields, $order_details_label) {
+  foreach ($fields as $field => $def)
+    if ($value = get_post_meta($order->id, $field, TRUE))
+      $s .= sprintf('<p class="form-field form-field-wide"><label for="%1$s">%2$s:</label><input type="text" id="%1$s" value="%3$s" readonly/></p>', esc_attr($field), esc_html($def['label']), esc_attr($value));
+  if ($s)
+    printf('<div class="clear"></div><h4 style="clear: both">%s</h4><div class="address">%s</div>', $order_details_label, $s);
+});
+
+// TODO add to order received page
